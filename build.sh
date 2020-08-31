f16d624b587f7ae540ffc6359d6cca50179c7c18
Hugo Sadok <sadok@cmu.edu>
2020-08-31 18:12:11 -0400

    Display backtrace when debugging

--- build.sh
+++ build.sh
@@ -404,7 +404,7 @@ case $TASK in
         export PATH="${BIN_DIR}:${PATH}"
         export LD_LIBRARY_PATH="${NATIVE_LIB_PATH}:${DPDK_LD_PATH}:${TOOLS_BASE}:${LD_LIBRARY_PATH}"
         sudo env PATH="$PATH" LD_LIBRARY_PATH="$LD_LIBRARY_PATH" LD_PRELOAD="$LD_PRELOAD" \
-            rust-gdb --args $executable "$@"
+            RUST_BACKTRACE=1 rust-gdb --args $executable "$@"
         ;;
     update_rust)
         _BUILD_UPDATE_=1
