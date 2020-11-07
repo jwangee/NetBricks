51abf8c9940c1aa4dda8aaec37d14d099e10a060
Tamas Levai <levait@tmit.bme.hu>
2020-11-07 14:28:51 +0100

    Add placeholders for nf chains to evaluate

--- examples.sh
+++ examples.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 # Lists all the examples in Bess. This is used by the build script.
+# export examples=(
+#         test/framework-test
+#         test/delay-test
+#         test/shutdown-test
+#         test/chain-test
+#         test/lpm
+#         test/lpm-embedded
+#         test/nat
+#         test/maglev
+#         test/tcp_check
+#         test/sctp-test
+#         test/config-test
+#         test/reset-parse
+#         test/tcp_reconstruction
+#         test/acl-fw
+#         test/dpi
+#         test/packet_generation
+#         test/packet_test
+#         test/embedded-scheduler-test
+#         test/embedded-scheduler-dependency-test
+#         test/tcp_payload
+#         test/macswap
+# )
 export examples=(
-        test/framework-test
-        test/delay-test
-        test/shutdown-test
-        test/chain-test
-        test/lpm
-        test/lpm-embedded
-        test/nat
-        test/maglev
-        test/tcp_check
-        test/sctp-test
-        test/config-test
-        test/reset-parse
-        test/tcp_reconstruction
-        test/acl-fw
-        test/dpi
-        test/packet_generation
-        test/packet_test
-        test/embedded-scheduler-test
-        test/embedded-scheduler-dependency-test
-        test/tcp_payload
-        test/macswap
+    faas-nfv/vlanpop-lpm
+    faas-nfv/acl-urlfilter-chacha
+    faas-nfv/acl-distribnat
 )
-
