2687138e368d8bae15a10a96ee236553c2263398
Tamas Levai <levait@tmit.bme.hu>
2020-11-21 15:46:59 +0000

    Make macswap work on phy ports

--- faas-nfv/macswap/src/main.rs
+++ faas-nfv/macswap/src/main.rs
@@ -6,27 +6,28 @@ extern crate getopts;
 extern crate rand;
 extern crate time;
 use self::nf::*;
-use e2d2::config::{basic_opts, read_matches};
+use e2d2::allocators::CacheAligned;
+use e2d2::config::*;
 use e2d2::interface::*;
 use e2d2::operators::*;
 use e2d2::scheduler::*;
 use std::env;
-use std::fmt::Display;
-use std::process;
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 mod nf;
 
-fn test<T, S>(ports: Vec<T>, sched: &mut S)
-where
-    T: PacketRx + PacketTx + Display + Clone + 'static,
-    S: Scheduler + Sized,
-{
-    for port in &ports {
-        println!("Receiving port {}", port);
-    }
+const CONVERSION_FACTOR: f64 = 1000000000.;
 
+fn test<S: Scheduler + Sized>(ports: Vec<CacheAligned<PortQueue>>, sched: &mut S) {
+    for port in &ports {
+        println!(
+            "Receiving port {} rxq {} txq {}",
+            port.port.mac_address(),
+            port.rxq(),
+            port.txq()
+        );
+    }
     let pipelines: Vec<_> = ports
         .iter()
         .map(|port| macswap(ReceiveBatch::new(port.clone())).send(port.clone()))
@@ -38,50 +39,55 @@ where
 }
 
 fn main() {
-    let mut opts = basic_opts();
-    opts.optopt(
-        "",
-        "dur",
-        "Test duration",
-        "If this option is set to a nonzero value, then the \
-         test will exit after X seconds.",
-    );
-
     let args: Vec<String> = env::args().collect();
+
+    let opts = basic_opts();
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
         Err(f) => panic!(f.to_string()),
     };
-    let mut configuration = read_matches(&matches, &opts);
-    configuration.pool_size = 255;
+    let configuration = read_matches(&matches, &opts);
 
-    let test_duration: u64 = matches
-        .opt_str("dur")
-        .unwrap_or_else(|| String::from("0"))
-        .parse()
-        .expect("Could not parse test duration");
+    let mut config = initialize_system(&configuration).unwrap();
+    config.start_schedulers();
 
-    match initialize_system(&configuration) {
-        Ok(mut context) => {
-            context.start_schedulers();
+    config.add_pipeline_to_run(Arc::new(move |p, s: &mut StandaloneScheduler| test(p, s)));
+    config.execute();
 
-            context.add_pipeline_to_run(Arc::new(move |p, s: &mut StandaloneScheduler| test(p, s)));
-            context.execute();
-
-            if test_duration != 0 {
-                thread::sleep(Duration::from_secs(test_duration));
-            } else {
-                loop {
-                    thread::sleep(Duration::from_secs(1));
+    let mut pkts_so_far = (0, 0);
+    let mut last_printed = 0.;
+    const MAX_PRINT_INTERVAL: f64 = 30.;
+    const PRINT_DELAY: f64 = 15.;
+    let sleep_delay = (PRINT_DELAY / 2.) as u64;
+    let mut start = time::precise_time_ns() as f64 / CONVERSION_FACTOR;
+    let sleep_time = Duration::from_millis(sleep_delay);
+    println!("0 OVERALL RX 0.00 TX 0.00 CYCLE_PER_DELAY 0 0 0");
+    loop {
+        thread::sleep(sleep_time); // Sleep for a bit
+        let now = time::precise_time_ns() as f64 / CONVERSION_FACTOR;
+        if now - start > PRINT_DELAY {
+            let mut rx = 0;
+            let mut tx = 0;
+            for port in config.ports.values() {
+                for q in 0..port.rxqs() {
+                    let (rp, tp) = port.stats(q);
+                    rx += rp;
+                    tx += tp;
                 }
             }
-        }
-        Err(ref e) => {
-            println!("Error: {}", e);
-            if let Some(backtrace) = e.backtrace() {
-                println!("Backtrace: {:?}", backtrace);
+            let pkts = (rx, tx);
+            let rx_pkts = pkts.0 - pkts_so_far.0;
+            if rx_pkts > 0 || now - last_printed > MAX_PRINT_INTERVAL {
+                println!(
+                    "{:.2} OVERALL RX {:.2} TX {:.2}",
+                    now - start,
+                    rx_pkts as f64 / (now - start),
+                    (pkts.1 - pkts_so_far.1) as f64 / (now - start)
+                );
+                last_printed = now;
+                start = now;
+                pkts_so_far = pkts;
             }
-            process::exit(1);
         }
     }
 }
--- faas-nfv/macswap/src/nf.rs
+++ faas-nfv/macswap/src/nf.rs
@@ -5,7 +5,6 @@ pub fn macswap<T: 'static + Batch<Header = NullHeader>>(
     parent: T,
 ) -> TransformBatch<MacHeader, ParsedBatch<MacHeader, T>> {
     parent.parse::<MacHeader>().transform(box move |pkt| {
-        assert!(pkt.refcnt() == 1);
         let hdr = pkt.get_mut_header();
         hdr.swap_addresses();
     })
